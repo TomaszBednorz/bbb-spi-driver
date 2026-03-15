86684253c9e0e28e7119e5403c21e1068e3ab70f
Tomasz Bednorz <tomekbed99@gmail.com>
2026-03-15 20:54:19 +0100

    Driver: Start of IIO device allocation

--- driver/imu_lsm6dso.c
+++ driver/imu_lsm6dso.c
@@ -3,8 +3,6 @@
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/spi/spi.h>
-#include <linux/regmap.h>
-#include <linux/iio/iio.h>
 
 #include "imu_lsm6dso_cfg.h"
 
@@ -13,30 +11,76 @@ static const struct regmap_config imu_lsm6dsx_regmap_config = {
 	.val_bits = 8,
 };
 
-// update hardcoded values
-// add some information as debug info (init sucessful, etc.)
-// maybe new file for communication?
-
 static int imu_lsm6dso_check_whoami(struct imu_lsm6dso_data *data)
 {
 	int err;
     unsigned int reg_val;
 
-	err = regmap_read(data->regmap, 0x0F, &reg_val);
+	err = regmap_read(data->regmap, IMU_LSM6DSO_REG_WHO_AM_I_ADDR, &reg_val);
 
 	if (err < 0) {
 		dev_err(data->dev, "Failed to read whoami register\n");
 		return err;
 	}
 
-	if (reg_val != 0x6C) {
-		// log
+	if (reg_val != IMU_LSM6DSO_REG_WHO_AM_I_VALUE) {
+		dev_err(data->dev, "Unexpected WHOAMI value: 0x%02X\n", reg_val);
 		return -ENODEV;
 	}
 
 	return 0;
 }
 
+static int imu_lsm6dso_alloc_iiodev(struct imu_lsm6dso_data *data)
+{
+	struct imu_lsm6dso_sensor *sensor;
+
+	data->iio_dev = devm_iio_device_alloc(data->dev, sizeof(*sensor));	
+
+	if(NULL == data->iio_dev)
+	{
+		dev_err(data->dev, "Failed to allocate IIO device\n");
+		return -ENOMEM;
+	}	
+
+
+	data->iio_dev->name = "imu-lsm6dso"; // 2 - accel and gyrp
+
+	// Configura accel and guro channels here (not implemented in this snippet)
+	// Add a configuration for Accel and gyro
+
+	iio_dev->modes = INDIO_DIRECT_MODE;
+	iio_dev->available_scan_masks = st_lsm6dsx_available_scan_masks;
+	iio_dev->channels = hw->settings->channels[id].chan;
+	iio_dev->num_channels = hw->settings->channels[id].len;
+
+	sensor = iio_priv(iio_dev);
+	sensor->id = id;
+	sensor->hw = hw;
+	sensor->odr = hw->settings->odr_table[id].odr_avl[0].milli_hz;
+	sensor->gain = hw->settings->fs_table[id].fs_avl[0].gain;
+	sensor->watermark = 1;
+
+	switch (id) {
+	case ST_LSM6DSX_ID_ACC:
+		iio_dev->info = &st_lsm6dsx_acc_info;
+		scnprintf(sensor->name, sizeof(sensor->name), "%s_accel",
+			  name);
+		break;
+	case ST_LSM6DSX_ID_GYRO:
+		iio_dev->info = &st_lsm6dsx_gyro_info;
+		scnprintf(sensor->name, sizeof(sensor->name), "%s_gyro",
+			  name);
+		break;
+	default:
+		return NULL;
+	}
+	iio_dev->name = sensor->name;
+
+	return iio_dev;
+
+	return 0;
+}
 
 static int imu_lsm6dso_probe(struct spi_device *spi)
 {
@@ -48,25 +92,23 @@ static int imu_lsm6dso_probe(struct spi_device *spi)
 
     /* Initialize the SPI regmap */
 	regmap = devm_regmap_init_spi(spi, &imu_lsm6dsx_regmap_config);
-	if (IS_ERR(regmap)) {
+	if (IS_ERR(regmap)) 
+	{
 		dev_err(&spi->dev, "Failed to register spi regmap %ld\n", PTR_ERR(regmap));
 		return PTR_ERR(regmap);
 	}
 
     /* Allocate driver data structure */
 	imu_data = devm_kzalloc(&spi->dev, sizeof(*imu_data), GFP_KERNEL);
-	if (!imu_data)
+	if (NULL == imu_data)
     {
         dev_err(&spi->dev, "Failed to allocate driver data\n");
 		return -ENOMEM;
     }
 	dev_set_drvdata(&spi->dev, (void *)imu_data);
 
-    // mutexes?
-
     imu_data->dev = &spi->dev;
     imu_data->regmap = regmap;
-
 
     /* Check the WHOAMI register to verify communication with the device */
 	if (err < imu_lsm6dso_check_whoami(imu_data))
@@ -74,6 +116,10 @@ static int imu_lsm6dso_probe(struct spi_device *spi)
         return err;
     }
 		
+	if(err < imu_lsm6dso_alloc_iiodev(imu_data))
+	{
+		return err;
+	}
 
 
 
--- driver/imu_lsm6dso_cfg.h
+++ driver/imu_lsm6dso_cfg.h
@@ -2,12 +2,35 @@
 #define IMU_LSM6DSO_CFG_H
 
 #include <linux/device.h>
+#include <linux/iio/iio.h>
+#include <linux/regmap.h>
+
+#include <stdint.h>
 
 #define DRIVER_NAME "imu-lsm6dso"
+
+#define IMU_LSM6DSO_REG_WHO_AM_I_ADDR  (0x0F)
+
+
+#define IMU_LSM6DSO_REG_WHO_AM_I_VALUE  (0x6C)
+
 
 struct imu_lsm6dso_data {
     struct device *dev;
     struct regmap *regmap;
+    struct iio_dev *iio_dev;
+};
+
+/**
+ * struct imu_lsm6dso_sensor - IMU LSM6DS sensor instance
+ * @hw: Pointer to instance of struct imu_lsm6dso_data.
+ * @gain: Configured sensor sensitivity.
+ * @odr: Output data rate of the sensor [Hz].
+ */
+struct imu_lsm6dso_sensor {
+    struct imu_lsm6dso_data *data;
+    uint32_t gain;
+	uint32_t odr;
 };
 
 #endif /* IMU_LSM6DSO_CFG_H */
